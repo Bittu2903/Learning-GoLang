dcff8f7701d7e0a4af95bb6b62e3f0a3c1d3c1ee
Bittu2903 <singhbittu2903@gmail.com>
2024-01-14 18:45:47 +0530

    different prints and formats

--- main.go
+++ main.go
@@ -4,5 +4,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	age := "23"
+	name := "Bittu"
+	floatedAge := 23.89
+	// Println
+	fmt.Println("Hello World!")  // automatically add next line \n
+	
+	// Print
+	fmt.Print("first line \n")
+	fmt.Print("Second line \n")
+	fmt.Print("The age is ",age," and the name is ",name,"\n")
+
+	// Printf (Formatter String)
+	fmt.Printf("The format string with age %v and name %v \n",age, name)
+	fmt.Printf("The format string with age %q and name %q \n",age, name)
+	fmt.Printf("The format string with age %f and name %v \n",floatedAge, name)
+
+	// Sprintf (save formatted string)
+	str := fmt.Sprintf("The format string with age %f and name %v \n",floatedAge, name)
+	fmt.Print(str)
 }
