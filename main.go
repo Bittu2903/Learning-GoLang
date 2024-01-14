baffe7ce0109af4d3eb549ffd6b2f3f94d78866f
Bittu2903 <singhbittu2903@gmail.com>
2024-01-14 19:15:13 +0530

    l5-arrays-and-slices

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 // Run file using 'go run file_name.go'
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	// Arrays
+	var ages [3]int = [3]int{2,7,5}
+	var names = [3]string{"bittu","singh","kingh"}
+	courses := [3]string{"MCA","M.Tech"}
+	fmt.Println(ages,names,courses)
+
+	// Slices
+	city := []string{"Delhi","HR","UP"}
+	city = append(city,"MP")
+	sliceOne := city[1:]
+	fmt.Println(city,sliceOne)
+
+
 }
