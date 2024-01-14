a89131ca18dc8be33f98fa73e6bb069e6792b733
Bittu2903 <singhbittu2903@gmail.com>
2024-01-14 17:59:46 +0530

    integer and float with unsigned

--- main.go
+++ main.go
@@ -9,4 +9,16 @@ func main() {
 	var strThree string
 	myString := "My first String"
 	fmt.Println(strOne,strTwo,strThree, myString)
+
+	var firstInt = 25
+	var secondInt int = 129
+	fmt.Println(firstInt, secondInt) 
+	var thirdInt int8 = 26
+	var fourthInt int8 = -128
+	fifthInt := 23435
+	fmt.Println(thirdInt, fourthInt, fifthInt)
+
+	var firstFloat float32 = 6767.0767567575
+	var secondFloat float64 = 6767.0767567575
+	fmt.Println(firstFloat, secondFloat)
 }
