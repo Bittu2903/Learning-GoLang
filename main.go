2066b5b9695fe2ef4eb92478718e7ba60d219d86
Bittu2903 <singhbittu2903@gmail.com>
2024-01-15 13:45:07 +0530

    Basic server in go

--- main.go
+++ main.go
@@ -1,8 +1,33 @@
 // Run file using 'go run file_name.go'
+// Creating basic server in GoLang
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+func getHome(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello from getHome")
+	io.WriteString(w,"Hello from get Home function")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello from getHello")
+	io.WriteString(w,"Hello from get Hello function")
+}
+
+func getIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello from getIndex")
+	io.WriteString(w,"Hello from get Index function")
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/",getHome)
+	http.HandleFunc("/hello",getHello)
+	http.HandleFunc("/index",getIndex) 
+	err := http.ListenAndServe(":3333",nil)
+	if err != nil{
+		fmt.Println("The error occured is ",err)
+	}
 }
