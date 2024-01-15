bfc6dcd96b81a9eceafd3b0784a2374e8e8b3def
Bittu2903 <singhbittu2903@gmail.com>
2024-01-15 22:12:12 +0530

    basic intro to loops

--- main.go
+++ main.go
@@ -5,4 +5,21 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!")
+	for x := 0; x < 5; x++ {
+		fmt.Println("value of x is",x)
+	}
+
+	names := []string{"Bittu","Singh","Kingh"}
+	for index, value := range names {
+		value = "new value"
+		fmt.Printf("The value at index %v is %v \n",index,value)
+		value = "newest value"
+		names[index] = "Raja"
+	}
+
+	for _, value := range names {
+		fmt.Printf("The value is %v \n",value)
+	}
+
+	fmt.Println(names)
 }
