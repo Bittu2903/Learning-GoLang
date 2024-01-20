f41fe317df01878ade7f1ec6dafbb39f012f55c0
Bittu2903 <singhbittu2903@gmail.com>
2024-01-20 23:50:45 +0530

    maps in go

--- main.go
+++ main.go
@@ -4,5 +4,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	languages := make(map[string] string)
+
+	languages["JS"] = "Javascript"
+	languages["PY"] = "Python"
+	languages["SQL"] = "Structured Query Language"
+
+	fmt.Println("List of all languages is: ",languages)
+	fmt.Println("JS languages is: ",languages["JS"])
+	delete(languages,"SQL")
+	fmt.Println("List of all languages is: ",languages)
+
+	// loops in maps
+	for _,value := range languages {
+		fmt.Printf("For key v, the value is %v\n",value)
+	}
 }
